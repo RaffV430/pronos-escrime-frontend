acc0caa8c6cf6091e4a808fd23a70ac3bf605d37
RaffV430 <raffouirutnev@yahoo.fr>
2026-07-26 00:10:19 +0200

    Test localhost pro prod

--- src/api.js
+++ src/api.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+// Si on est en local, on tape sur localhost:5000/api
+// Sinon, on tape sur ton URL Render en production
+const baseURL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? 'http://localhost:5000/api'
+  : 'https://pronos-escrime.onrender.com/api';
+
 const API = axios.create({
-  baseURL: 'https://pronos-escrime.onrender.com/api',
+  baseURL: baseURL,
 });
 
 export default API;
